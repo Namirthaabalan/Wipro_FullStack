bca1ec56d4e61c90576bce0cd192eda0150dab4d
Namirthaa <namirthaab@gmail.com>
2025-07-17 15:57:54 +0530

    ready

---
+++ Java_Fundamentals/Hands_on/fundamentals3.java
@@ -0,0 +1,17 @@
+public class fundamentals3 {
+
+	public static void main(String[] args) {/*
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a");
+		int a= sc.nextInt();
+		System.out.println("Enter the value of b");
+		int b = sc.nextInt();
+		int result = a+b;
+		System.out.println("The sum of "+a+" and "+b+" is "+ result);*/
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+
+	}
+
+}
