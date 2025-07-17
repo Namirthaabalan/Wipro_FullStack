b06c7761b0533694544c4f5770229167544a2f66
Namirthaa <namirthaab@gmail.com>
2025-07-17 15:39:46 +0530

    ready

---
+++ Java_Fundamentals/Hands_on/fundamentals1.java
@@ -0,0 +1,21 @@
+//import java.util.Scanner;
+public class fundamentals1 {
+
+	public static void main(String[] args) {/* BASIC PROGRAM
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string1");
+		String s1 = sc.nextLine();
+		System.out.println("Enter the string2");
+		String s2 = sc.nextLine();
+		System.out.print(s1 + " technologies " + s2 );
+
+	}
+		//BY USING THE COMMAND LINE ARGUMENT
+		String s1 = args[0];
+		String s2 = args[1];
+		System.out.println(s1 + " Technologies "+ s2);
+		*/
+		System.out.println(args[0] + " Technologies " + args[1]);
+	}
+
+}
